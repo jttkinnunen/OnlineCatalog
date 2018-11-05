1eb1be6008f7ebe11f10ebdacc3f27d75fb49e74
Joni <joni.laitala@cloudasset.com>
2018-11-05 16:08:04 +0200

    Routing added: "/activate?key=..." opens activation view

--- front-end/src/components/content-components/ActivateUser.js
+++ front-end/src/components/content-components/ActivateUser.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Form, Input, FormGroup, Label, Button, Jumbotron} from "reactstrap";
+import { Form, Input, FormGroup, Button, Jumbotron} from "reactstrap";
+import queryString from 'query-string';
+import postJsonRequest from '../../App.js';
+
+
+// TODO: What if user tries to activate multiple times? proper checks and output missing
+// TODO: prepare for cases where user by this id does not exist etc. coordinate with back-end
 
 class ActivateUser extends React.Component {
     constructor(props) {
@@ -7,6 +13,7 @@ class ActivateUser extends React.Component {
         this.state = {
             pass_first: '',
             pass_second: '',
+            activation_result: '',
         };
         this.handlePassOneChange = this.handlePassOneChange.bind(this);
         this.handlePassTwoChange = this.handlePassTwoChange.bind(this);
@@ -26,39 +33,77 @@ class ActivateUser extends React.Component {
     }
 
     handleSubmit() {
-        this.props.setView("login");
+        if (this.state.pass_first === this.state.pass_second) {
+
+            this.setState({
+                pass_first: '',
+                pass_second: '',
+                activation_result: 'Aktivoidaan...'
+            });
+
+            postJsonRequest('/setPassword',
+                    {
+                        newPassword: this.state.pass_first,
+                        resetToken: queryString.parse(this.props.location.search).key,
+                    }
+                ).then((responseJson) => {
+
+                    // TODO: ota vastauksesta tieto onnistuiko
+                    // responseJson.jotain
+                    // if (responseJson.hasOwnProperty('jokuelementti'))
+
+                    this.setState({
+                        pass_first: "",
+                        pass_second: ""
+                    });
+
+                })
+                .catch(err => {
+                    this.setState({
+                        debugval: this.state.debugval + " Error-fetching-articles:" + err,
+                        activation_result: 'Yhteysongelma'
+                    })
+                })
+        }
+        else
+            this.setState({
+                activation_result: 'Salasanat eivät täsmää'
+            });
     }
 
     render() {
         return(
-            <Jumbotron>
-                <Form>
-                    <h5 className = "white-h5">Aktivoi tunnuksesi</h5>
-                    <p>
-                        <h6 className = "white-h6">Tervetuloa käyttämään Matskua, Placeholder! Aseta itsellesi salasana ja pääset heti hommiin.</h6>
-                    </p>
+            <div className="App App-header">
+                    <Jumbotron className = "align-self-center">
+                        <Form>
+                            <h5 className = "white-h5">Aktivoi tunnuksesi</h5>
+                            <p>
+                                <h6 className = "white-h6">Tervetuloa käyttämään Matskua, Placeholder! Aseta itsellesi salasana ja pääset heti hommiin.</h6>
+                                <div className = "white-h6">(Testiä varten) avain: {queryString.parse(this.props.location.search).key} </div>
+                            </p>
 
-                    <FormGroup>
-                        <span>
-                            <Input type="password" name="pass1" id="Pass1" placeholder="Salasana" value = {this.state.pass_first} onChange = {this.handlePassOneChange}/>
-                        </span>
-                    </FormGroup>
-                    <FormGroup>
-                        <span>
-                            <Input type="password" name="pass2" id="Pass2" placeholder="Salasana uudestaan" value = {this.state.pass_second} onChange = {this.handlePassTwoChange}/>
-                        </span>
-                    </FormGroup>
-                    <br/>
+                            <FormGroup>
+                                <span>
+                                    <Input type="password" name="pass1" id="Pass1" placeholder="Salasana" value = {this.state.pass_first} onChange = {this.handlePassOneChange}/>
+                                </span>
+                            </FormGroup>
+                            <FormGroup>
+                                <span>
+                                    <Input type="password" name="pass2" id="Pass2" placeholder="Salasana uudestaan" value = {this.state.pass_second} onChange = {this.handlePassTwoChange}/>
+                                </span>
+                            </FormGroup>
+                            <br/>
 
-                    <FormGroup>
+                            <FormGroup>
 
-                        <Button className="btn btn-success" onClick={this.handleSubmit}>Aktivoi</Button>
+                                <Button className="btn btn-success" onClick={this.handleSubmit}>Aktivoi</Button>
 
-                    </FormGroup>
-                    <br/>
+                            </FormGroup>
+                            <br/>
 
-                </Form>
-            </Jumbotron>
+                        </Form>
+                    </Jumbotron>
+            </div>
         );
     }
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
 import './css/index.css';
 import'../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter  } from 'react-router-dom'
+import ActivateUser from './components/content-components/ActivateUser.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+    <BrowserRouter>
+        <div>
+            <Route path="/activate" component={ActivateUser}/>
+            <Route exact path="/" component={App}/>
+        </div>
+    </BrowserRouter>
+), document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
